c7f07741a79b430563165cb8f213f9b59124d7a5
MuhamadIffatullathoif <iffatlathoif1997@gmail.com>
2024-01-02 08:56:08 +0700

    02. Create Domain Model Data

---
+++ src/main/java/com/iffat/springboot/restapi/domain/Beer.java
@@ -0,0 +1,50 @@
+package com.iffat.springboot.restapi.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+import org.hibernate.type.SqlTypes;
+
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+
+    @Id
+    @UuidGenerator
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
+    private UUID id;
+
+    @Version
+    private Long version;
+
+    private String beerName;
+    private BeerStyleEnum beerStyleEnum;
+    private String upc;
+
+    private Integer quantityOnHand;
+    private BigDecimal price;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createdDate;
+
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+}
---
+++ src/main/java/com/iffat/springboot/restapi/domain/BeerStyleEnum.java
@@ -0,0 +1,5 @@
+package com.iffat.springboot.restapi.domain;
+
+public enum BeerStyleEnum {
+    LAGER, PILSNER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
